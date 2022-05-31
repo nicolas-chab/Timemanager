006faffbafee2f0446c56ea3bc802d57355ea009
nicolas-chab <101563616+nicolas-chab@users.noreply.github.com>
2022-05-31 15:15:55 -0300

    Initial commit

---
+++ Assets/Timemanager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timemanager : MonoBehaviour
+{
+    public Text txtTime;
+    public Text txtTimeFloored;
+    public Text txtTimeCustom;
+    public float customTime;
+    public bool isCounting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        customTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float elapsedTime = Time.time;
+        txtTime.text = elapsedTime.ToString();
+        txtTimeFloored.text =Mathf.Floor(elapsedTime).ToString();
+        txtTimeCustom.text = customTime.ToString();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isCounting = !isCounting;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            customTime = 0;
+        }
+        if (isCounting)
+        {
+            customTime += Time.deltaTime;
+        }
+    }
+}
